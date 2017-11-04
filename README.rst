ea54e278a81975540b9028c4a285c4a3095ef6d3
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2017-11-04 17:43:36 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+Pepper package for Python 2.7
+==============================
+
+A module for controlling Pepper, the humanoid robot and his friends
+https://www.ald.softbankrobotics.com/en/robots
+
+This module is inspired by the turtle package of Python.
+
+It is based on the naoqi module, so it works only with the 32 bit
+version of Python 2.7.
+
+You need to set the correct IP in the pepper_data.py file.
+
+These files can be found at
+https://github.com/albainnovar/pepper
+
---
+++ pepper_turtle.py
@@ -0,0 +1,67 @@
+# encoding: utf-8
+from __future__ import division
+from naoqi import ALProxy
+from pepper_data import IP, PORT
+from math import radians
+
+
+class Turtle:
+    def __init__(self, ip=IP, port=PORT, ask_ok=True):
+        self.tts = ALProxy("ALTextToSpeech", ip, port)
+        self.motion = ALProxy("ALMotion", ip, port)
+        self.ip = ip
+        self.port = port
+        self.tts.say("Hello! I'm ready to work. You can control me now.")
+        self.ask_ok = ask_ok
+
+    def _movement(self, template, measure, x=0, y=0, degree=0):
+        if not ask_ok():
+            return
+        self.tts.say(template.format(measure))
+        can_move = self.motion.moveTo(x/100, y/100, radians(degree))
+        if not can_move:
+            self.say('There is an obstacle. I can not move.')
+
+
+    def forward(self, distance):
+        "Go `distance` in centimeters."
+        if out_of_interval(0, 200, distance):
+            return
+        self._movement("I go forward {} centimeters.", distance, x=distance)
+
+    def backward(self, distance):
+        "Go `distance` in centimeters."
+        if out_of_interval(0, 200, distance):
+            return
+        self._movement("I go backward {} centimeters.", distance, x=-distance)
+
+    def left(self, degree):
+        "Turn right `degree` degrees."
+        if out_of_interval(0, 180, degrees):
+            return
+        self._movement("I turn left {} degrees.", degree, degree=degree)
+
+    def right(self, degree):
+        "Turn right `degree` degrees."
+        if out_of_interval(0, 180, degrees):
+            return
+        self._movement("I turn right {} degrees.", degree, degree=-degree)
+
+    def move_left(self, distance):
+        "Go `distance` in centimeters."
+        if out_of_interval(0, 40, distance):
+            return
+        self._movement("I go left {} centimeters.", distance, y=distance)
+
+    def move_right(self, distance):
+        "Go `distance` in centimeters."
+        if out_of_interval(0, 40, distance):
+            return
+        self._movement("I go right {} centimeters.", distance, y=-distance)
+
+    def say(self, text):
+        self.tts.say(text)
+
+    def out_of_interval(self, min, max, value):
+        if value > max of value < min:
+            self.say("This value should be between {} and {}.".format(min, max))
